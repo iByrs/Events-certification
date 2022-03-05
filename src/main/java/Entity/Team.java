b46d11f62509f6eb95eb879d3717f00d110da099
iByrs <vincent.pastor@hotmail.com>
2022-03-05 14:57:30 +0100

    Completate la classe Team

--- src/main/java/Entity/Team.java
+++ src/main/java/Entity/Team.java
@@ -9,6 +9,16 @@ public class Team {
     private Worker entity1;
     private Worker entity2;
 
+    /*
+    * Il metodo deve permettere di far interaggire le entità tra loro e nel frattempo inviare i messaggi
+    * */
+    public Team(Long id, Worker driver, Worker entity1, Worker entity2) {
+        this.id = id;
+        this.driver = driver;
+        this.entity1 = entity1;
+        this.entity2 = entity2;
+    }
+
     public void sendEvents() throws InterruptedException {
         for(int i = 0; i < 4; i++) {
             driver.sendEvent();
@@ -16,5 +26,10 @@ public class Team {
             entity2.sendEvent();
             Thread.sleep(1000);
         }
+    }
+
+    public String teamToString() {
+        String msg = "Team "+id.toString()+" "+entity1.toString()+" "+entity2.toString()+" "+driver.toString();
+        return msg;
     }
 }
