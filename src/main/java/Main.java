a002b0fe3ee4361b97d42458a1007bd0372e56a5
iByrs <vincent.pastor@hotmail.com>
2022-03-16 15:03:19 +0100

    Aggiunto il repository

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         Logger.clean();
         Facade facade = new Facade();
-        for(int i = 0; i < 100; i++) {
+        for(int i = 0; i < 6; i++) {
             facade.sendEvent();
             Thread.sleep(1000);
         }
